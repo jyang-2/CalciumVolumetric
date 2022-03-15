4b8dd47a05e8a9c3eabdcc3cfdb3b609e9801673
remy <jyang2@caltech.edu>
2022-03-15 13:26:05 -0700

    Edited rasterplot.py to remove old code

--- src/rasterplot.py
+++ src/rasterplot.py
@@ -10,33 +10,6 @@ suite2p_ops = {'n_components': 1, 'n_X': 100, 'alpha': 1., 'K': 1.,
                'nPC': 200, 'constraints': 2, 'annealing': True, 'init': 'pca',
                }
 
-"""
-Parameters
------------
-Rastermap first takes the specified PCs of the data, and then embeds them into n_X clusters. It returns upsampled 
-cluster identities (n_X x upsamp). Clusters are also computed across Y (n_Y) and smoothed, to help with fitting. 
-
-    n_components : int, optional (default: 2) dimension of the embedding space
-    n_X : int, optional (default: 40) size of the grid on which the Fourier modes are rasterized
-    nPC : nparray, int, optional (default: 400) how many of the top PCs to use during optimization
-    alpha : float, optional (default: 1.0) exponent of the power law enforced on component n as: 1/(K+n)^alpha
-    K : float, optional (default: 1.0) additive offset of the power law enforced on component n as: 1/(K+n)^alpha
-    init : initialization of algorithm (default: 'pca') can use 'pca', 'random', or a matrix n_samples x n_components
-
-    # hidden 'init' allowed values: 'pca', 'dbisomap', 'random', 'laplacian', 'lle'
-
-Outputs
--------
-Rastermap model has the following attributes after running 'fit':
-
-    embedding : array-like, shape (n_samples, n_components) Stores the embedding vectors.
-    u,sv,v : singular value decomposition of data S, potentially with smoothing
-    isort1 : sorting along first dimension (n_samples) of matrix
-    cmap : correlation of each item with all locations in the embedding map (before upsampling)
-    A : PC coefficients of each Fourier mode
-
-"""
-
 
 def suite2p_reordered_trial_display(sp, df_stimulus, frametimes, nbin=None, nplotrows=400,
                                     pre_stim=-20, post_stim=40,
